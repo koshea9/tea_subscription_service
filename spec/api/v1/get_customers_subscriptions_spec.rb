a79eb4c79d153a500516bf82ae89ac6b9fd6ec4c
koshea9 <keegan.oshea9@gmail.com>
2021-08-03 16:22:05 -0600

    add before each block to test

--- spec/api/v1/get_customers_subscriptions_spec.rb
+++ spec/api/v1/get_customers_subscriptions_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "All subscriptions for a customer" do
   describe "happy path" do
+    before :each do
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @tea_1, @tea_2, @tea_3 = create_list(:tea, 3)
+      @subscription_1 = create(:subscription, customer_id: @customer_1.id, tea_id: @tea_1.id)
+      @subscription_2 = create(:subscription, customer_id: @customer_1.id, tea_id: @tea_2.id)
+      @subscription_3 = create(:subscription, customer_id: @customer_1.id, tea_id: @tea_3.id)
+      @subscription_4 = create(:subscription, customer_id: @customer_2.id, tea_id: @tea_1.id)
+    end
     it "returns all a customer's subscriptions" do
-      customer_1 = create(:customer)
-      customer_2 = create(:customer)
-      tea_1, tea_2, tea_3 = create_list(:tea, 3)
-      subscription_1 = create(:subscription, customer_id: customer_1.id, tea_id: tea_1.id)
-      subscription_2 = create(:subscription, customer_id: customer_1.id, tea_id: tea_2.id)
-      subscription_3 = create(:subscription, customer_id: customer_1.id, tea_id: tea_3.id)
-      subscription_4 = create(:subscription, customer_id: customer_2.id, tea_id: tea_1.id)
 
-      get "/api/v1/customers/#{customer_1.id}/subscriptions"
+      get "/api/v1/customers/#{@customer_1.id}/subscriptions"
 
       expect(response).to be_successful
       subscriptions = JSON.parse(response.body, symbolize_names: true)
