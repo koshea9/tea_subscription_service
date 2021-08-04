9b7c39abc8d0bd2721bef4ff33dfa4b01c655b25
koshea9 <keegan.oshea9@gmail.com>
2021-08-03 19:30:12 -0600

    implement endpoint to update customers subscription to cancelled status

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/customers/:id/subscriptions", to: "subscriptions#index"
+      patch "/subscriptions/:id", to: "subscriptions#update"
     end
   end
 end
